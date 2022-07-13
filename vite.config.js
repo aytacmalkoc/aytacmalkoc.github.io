61b1eb8e31f15f98707e7e559b1048daa742718c
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-13 04:05:18 +0300

    Update @ alias

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': './src',
       'node-fetch': 'node-fetch/browser.js',
     },
     extensions: ['.jsx', '.js'],
