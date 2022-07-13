ff2613c148a61c86be809f7170d6858680035373
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-13 04:07:45 +0300

    Delete base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': './src',
       'node-fetch': 'node-fetch/browser.js',
     },
     extensions: ['.jsx', '.js'],
