4df4603ea17e3e88509a195750dde29277c5ec51
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-13 03:51:21 +0300

    Update exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,8 @@
-export default {
-    Layout: require('./Layout'),
-    Header: require('./Header'),
-    Hero: require('./Hero'),
-    Footer: require('./Footer'),
-}
+export { default as Layout } from './Layout'
+export { default as Hero } from './Hero'
+export { default as Repositories } from './Repositories'
+export { default as Skills } from './Skills'
+export { default as Educations } from './Educations'
+export { default as Experiences } from './Experiences'
+export { default as Footer } from './Footer'
+export { default as BackTop } from './BackTop'
