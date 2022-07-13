a5007c7336385db528aba955dca28c6c79e6405a
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-13 03:37:57 +0300

    Change API requests from graphql to octokit.js

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -1,66 +1,56 @@
 import React, { useState, useEffect } from 'react'
-import { request, gql } from 'graphql-request'
+import { useStorage } from '../hooks/useStorage'
+import getColor from 'github-lang-colors'
+import { Octokit } from 'octokit'
+
 import RepositoryItem from './RepositoryItem'
 
 export default function Repositories() {
 
     const [repositories, setRepositories] = useState([])
+    const [cachedRepositories, setCachedRepositories] = useStorage('repositories')
 
     const fetchRepositories = async () => {
 
-        const query = gql`{
-            user(login: "aytacmalkoc") {
-                pinnedItems(first: 6) {
-                    edges {
-                        node {
-                            ... on Repository {
-                                id
-                                nameWithOwner
-                                description
-                                url
-                                createdAt
-                                updatedAt
-                                openGraphImageUrl
-                                primaryLanguage {
-                                    id
-                                    name
-                                    color
-                                }
-                                languages(first: 5) {
-                                    edges {
-                                        node {
-                                            id
-                                            name
-                                            color
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }`
+        if (cachedRepositories) {
+            setRepositories(cachedRepositories)
+        } else {
+            let repos = []
 
-        request({
-            url: import.meta.env.secrets.VITE_GITHUB_API_URL,
-            document: query,
-            requestHeaders: {
-                Authorization: `Bearer ${import.meta.env.secrets.VITE_GITHUB_ACCESS_TOKEN}`
-            }
-        }).then(data => setRepositories(data.user.pinnedItems.edges))
+            const octokit = new Octokit({
+                auth: import.meta.env.VITE_GITHUB_ACCESS_TOKEN || '',
+            })
+
+            await octokit.rest.repos.listForUser({
+                username: 'aytacmalkoc',
+                sort: 'updated',
+                per_page: 6,
+                page: 1
+            }).then((res) => {
+                res.data.forEach(repo => {
+                    if (!repo.fork) {
+                        repos.push({
+                            id: repo.id,
+                            nameWithOwner: repo.full_name,
+                            url: repo.html_url,
+                            description: repo.description,
+                            language: {
+                                name: repo.language,
+                                color: getColor(repo.language)
+                            },
+                            archived: repo.archived,
+                        })
+                    }
+                })
+            })
+
+            setRepositories(repos)
+            setCachedRepositories(repos)
+        }
     }
 
     useEffect(() => {
-        const cache = JSON.parse(localStorage.getItem('cachedRepositories'))
-
-        if (cache && cache.length > 0) {
-            setRepositories(cache)
-        }
-        else {
-            fetchRepositories()
-            localStorage.setItem('cachedRepositories', JSON.stringify(repositories))
-        }
+        fetchRepositories()
     }, [])
 
     return (
@@ -71,8 +61,11 @@ export default function Repositories() {
                 </div>
                 <div className="portfolio-works">
                     {repositories.map(repo => (
-                        <RepositoryItem key={repo.node.id} repository={repo} />
+                        <RepositoryItem key={repo.id} repository={repo} />
                     ))}
+                </div>
+                <div className="col-12 text-center mt-60">
+                    <a href="https://github.com/aytacmalkoc" className="btn btn-outline-dark" target={'_blank'}>see more on github</a>
                 </div>
             </div>
         </div>
