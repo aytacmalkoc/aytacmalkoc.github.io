9db809a6cf391e83bdd668f415438df6052a25de
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-13 04:31:01 +0300

    Update import type

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Layout from './components/Layout'
+import { Layout } from './components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
