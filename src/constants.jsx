a5286dcbb0ee12b757f32614e50b18bff18328e6
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-14 07:37:27 +0300

    Add social media constants

---
+++ src/constants.jsx
@@ -0,0 +1,32 @@
+export const socialMediaLinks = [
+    {
+        id: 1,
+        label: 'Github',
+        icon: 'fa-brands fa-github',
+        url: 'https://www.github.com/aytacmalkoc',
+    },
+    {
+        id: 2,
+        label: 'Npm',
+        icon: 'fa-brands fa-npm',
+        url: 'https://www.npmjs.com/~aytacmalkoc',
+    },
+    {
+        id: 3,
+        label: 'Twitter',
+        icon: 'fa-brands fa-twitter',
+        url: 'https://www.twitter.com/jrd_aytacmalkoc',
+    },
+    {
+        id: 4,
+        label: 'Instagram',
+        icon: 'fa-brands fa-instagram',
+        url: 'https://www.instagram.com/aytacmalkoc35',
+    },
+    {
+        id: 5,
+        label: 'Email',
+        icon: 'fa-solid fa-envelope',
+        url: 'mailto:aytacmalkoc@protonmail.com'
+    }
+]
