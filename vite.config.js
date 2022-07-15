4e390aeb7d9c438cec2d60b8a3af5b8840800d70
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-15 07:57:28 +0300

    Add purgecss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import htmlPurge from 'vite-plugin-html-purgecss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    htmlPurge(),
+  ],
   resolve: {
     alias: {
       'node-fetch': 'node-fetch/browser.js',
