1f548158193467c6673a2d7d1cb672d8b26bed1d
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-15 07:23:20 +0300

    Initialize GA

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga';
 import { Layout } from './components';
+
+ReactGA.initialize(import.meta.env.VITE_APP_GA_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
