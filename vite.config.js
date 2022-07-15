3adae1bb7cfd47e55d221856b14d2f058bbcc33d
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-15 08:12:24 +0300

    Disable htmlPurge

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import htmlPurge from 'vite-plugin-html-purgecss'
+// import htmlPurge from 'vite-plugin-html-purgecss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    htmlPurge(),
+    // htmlPurge(),
   ],
   resolve: {
     alias: {
