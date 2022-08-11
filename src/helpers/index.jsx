5d0cecb383422036aae1c85a6f5c623a44200862
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-08-11 19:52:36 +0300

    Add scrollTo function

--- src/helpers/index.jsx
+++ src/helpers/index.jsx
@@ -1,1 +1,3 @@
 export const openFile = (file, target = '_blank') => window.open(file, target);
+
+export const scrollTo = (to, position, behavior = 'smooth') => window.scrollTo({ [to]: position, behavior });
