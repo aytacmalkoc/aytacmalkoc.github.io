cf32428c46134d134365aa47813831b4556cea0a
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-08-12 04:08:30 +0300

    Reformat experiences item with mapping

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { experiences } from '../constants';
 
 export default function Experiences() {
   return (
@@ -8,45 +9,24 @@ export default function Experiences() {
           <h2 className="sec-title">Experiences</h2>
         </div>
         <div className="experience-list">
-          <div className="single-experience">
-            <div className="row">
-              <div className="col-lg-4 col-xl-3 wow fadeInLeft">
-                <div className="experience-duration mb-3">
-                  <h5>August 2021 - Dec 2021</h5>
-                  <h4 className="text-black-50">upperearth digital</h4>
+          {experiences.map((experience) => (
+            <div className="single-experience" key={experience.id}>
+              <div className="row">
+                <div className="col-lg-4 col-xl-3 wow fadeInLeft">
+                  <div className="experience-duration mb-3">
+                    <h5>{experience.duration}</h5>
+                    <h4 className="text-black-50">{experience.company}</h4>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-8 col-xl-9 wow fadeInUp">
-                <div className="experience-details">
-                  <h4>Co-founder</h4>
-                  <p>
-                    I have carried out many corporate works in the design agency that I am a partner
-                    of. During this time, I learned a lot about company management and customer
-                    relations.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="single-experience">
-            <div className="row">
-              <div className="col-lg-4 col-xl-3 wow fadeInLeft">
-                <div className="experience-duration mb-3">
-                  <h5>Sep 2018 - June 2019</h5>
-                  <h4 className="text-black-50">Fikir İşler</h4>
-                </div>
-              </div>
-              <div className="col-lg-8 col-xl-9 wow fadeInUp">
-                <div className="experience-details">
-                  <h4>Software Intern</h4>
-                  <p>
-                    During my internship, I took part in many projects. I provided support for
-                    frontend design and backend management. I developed myself in human relations.
-                  </p>
+                <div className="col-lg-8 col-xl-9 wow fadeInUp">
+                  <div className="experience-details">
+                    <h4>{experience.title}</h4>
+                    <p>{experience.description}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
